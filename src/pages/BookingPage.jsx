5ec1ea729a12eb9803edec59217167f1d83977a0
PavelRakhman <pavel.rakhman.wdev@gmail.com>
2025-10-21 21:35:07 -0500

    Updated Booking page

--- src/pages/BookingPage.jsx
+++ src/pages/BookingPage.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import axios from 'axios';
+import { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalContext';
 
 function Booking() {
   const { register, handleSubmit, formState: { errors } } = useForm();
+  const { toggleSwitchState, setToggleSwitchState } = useContext(GlobalContext);
+
+  const handleFormInteraction = () => {
+    setToggleSwitchState(false);
+  };
 
   const onSubmit = async (data) => {
     const formData = new FormData();
+    data.subject = `New Booking request from ${data.fullName}`
+    formData.append('subject', data.subject);
     formData.append('full-name', data.fullName);
     formData.append('height', data.height);
     formData.append('email', data.email);
@@ -16,9 +25,8 @@ function Booking() {
     if (data.designFile1) formData.append('design-file-1', data.designFile1[0]);
     if (data.designFile2) formData.append('design-file-2', data.designFile2[0]);
     if (data.designFile3) formData.append('design-file-3', data.designFile3[0]);
-
     try {
-      const response = await axios.post(`http://formspree.io/f/xnngrbnj`, formData, {
+      const response = await axios.post(`https://formspree.io/f/xnngrbnj`, formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
@@ -35,16 +43,17 @@ function Booking() {
   };
 
   return (
-    <div className="booking-form-container">
+    <div className="booking-form-container" onClick={handleFormInteraction}>
       <form onSubmit={handleSubmit(onSubmit)}>
+        <input type="hidden" name="subject" value="New Booking Request from {{fullName}}" />
         <label htmlFor="fullName">Full Name:</label>
         <input
           type="text"
           id="fullName"
           {...register('fullName', { required: true })}
+          onFocus={handleFormInteraction}
         />
         {errors.fullName && <p>Full name is required</p>}
-
         <label htmlFor="height">Height:</label>
         <input
           type="text"
@@ -52,7 +61,6 @@ function Booking() {
           {...register('height', { required: true })}
         />
         {errors.height && <p>Height is required</p>}
-
         <label htmlFor="email">Email:</label>
         <input
           type="email"
@@ -60,7 +68,6 @@ function Booking() {
           {...register('email', { required: true })}
         />
         {errors.email && <p>Email is required</p>}
-
         <label htmlFor="phone">Contact Number:</label>
         <input
           type="tel"
@@ -68,41 +75,35 @@ function Booking() {
           {...register('phone', { required: true })}
         />
         {errors.phone && <p>Phone number is required</p>}
-
         <label htmlFor="designFile">Photo of the body part:</label>
         <input
           type="file"
           id="designFile"
           {...register('designFile')}
         />
-
         <label htmlFor="designFile1">Inspirational photo 1:</label>
         <input
           type="file"
           id="designFile1"
           {...register('designFile1')}
         />
-
         <label htmlFor="designFile2">Inspirational photo 2:</label>
         <input
           type="file"
           id="designFile2"
           {...register('designFile2')}
         />
-
         <label htmlFor="designFile3">Inspirational photo 3:</label>
         <input
           type="file"
           id="designFile3"
           {...register('designFile3')}
         />
-
         <label htmlFor="designDescription">Design Description:</label>
         <textarea
           id="designDescription"
           {...register('designDescription')}
         />
-
         <button type="submit" className="book-btn">Submit a Booking Request</button>
       </form>
     </div>
