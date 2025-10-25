8db276a1241d1a3a47f3dc98ca04e63c0593ead5
PavelRakhman <pavel.rakhman.wdev@gmail.com>
2025-10-25 17:54:22 -0500

    removed all unused components

--- src/components/InstagramButton.jsx
+++ src/components/InstagramButton.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FaInstagram } from "react-icons/fa";
-import { Link } from "react-router-dom";
+
 function InstagramButton() {
     return(
         <div className="instagram-button"><a href="https://www.instagram.com/sogdiana.sk/" target="_blank" rel="noopener noreferrer"><FaInstagram></FaInstagram></a></div>
